115890a1640a3886438c336386091d6a77653175
Enzo Rodrigues <enzorodrigues272@gmail.com>
2022-04-27 23:24:03 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -3,5 +3,5 @@ import socket
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 urlDigitada=input("Digite: ")
 s.connect((urlDigitada, 80))
-s.sendall(b"GET / HTTP/1.1\r\nHost:Banrisul.com.br\r\nAccept:text/html\r\n\r\n")
+s.sendall(b"GET / HTTP/1.1\r\nHost:google.com\r\nAccept:text/html\r\n\r\n")
 print(str(s.recv(4096), 'utf-8'))
